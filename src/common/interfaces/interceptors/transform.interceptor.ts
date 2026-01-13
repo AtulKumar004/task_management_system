ef6eccbba8f502510a13800f7115b27c3c94122d
krishna_atul <krishna.atul@scaleupally.io>
2026-01-13 17:44:37 +0530

    Implement project and task management with CRUD operations, add global exception handling, and enhance API response structure

---
+++ src/common/interfaces/interceptors/transform.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ApiResponse } from '../api-response.interface';
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<
+  T,
+  ApiResponse<T>
+> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<ApiResponse<T>> {
+    return next.handle().pipe(
+      map((data) => ({
+        success: true,
+        message: 'Operation successful', // You can make this dynamic if needed
+        statusCode: context.switchToHttp().getResponse().statusCode,
+        data: data,
+      })),
+    );
+  }
+}
